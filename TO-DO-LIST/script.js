02b70bc5f0138e1297cfb8ea9f9bcfd40bde871c
ranashish0401 <ashishgautamrana731@gmail.com>
2026-05-20 09:53:06 +0530

    Add files via upload
    A To-Do List App is a simple task management application used to organize daily activities and keep track of work that needs to be completed. Users can add tasks, mark them as completed, and delete unnecessary tasks.

---
+++ TO-DO-LIST/script.js
@@ -0,0 +1,57 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <span class="${task.completed ? 'completed' : ''}"
+                  onclick="toggleTask(${index})">
+                  ${task.text}
+            </span>
+
+            <button onclick="deleteTask(${index})">Delete</button>
+        `;
+
+        taskList.appendChild(li);
+    });
+}
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+
+    if (input.value.trim() === "") {
+        alert("Enter a task");
+        return;
+    }
+
+    tasks.push({
+        text: input.value,
+        completed: false
+    });
+
+    input.value = "";
+    saveTasks();
+    renderTasks();
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    saveTasks();
+    renderTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    saveTasks();
+    renderTasks();
+}
+
+renderTasks();
